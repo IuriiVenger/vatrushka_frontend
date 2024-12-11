d5ceada2b9f8d2687389e2e2fe8f9fece27e865d
Iurii Venger <venger.yuriy@icloud.com>
2024-12-11 17:49:14 +0100

    fix cardFullfillment props

--- src/components/AccountPageComponents/components/cardComponents/Fulfillment.tsx
+++ src/components/AccountPageComponents/components/cardComponents/Fulfillment.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 type TFulfillmentProps = {
   type: string;
   address: string;
-  time: string;
+  time: string | null;
   paymentMethod: string;
 };
 
@@ -11,7 +11,7 @@ const Fulfillment: FC<TFulfillmentProps> = ({ type, address, time, paymentMethod
   <div className="flex h-max w-full max-w-112 flex-col gap-3 rounded-2xl border border-borderSecondary p-6 max-lg:border-none max-lg:p-0 max-sm:gap-2">
     <h3 className="text-nowrap pb-2 text-xl font-medium leading-xl max-sm:text-lg max-sm:leading-lg">{type}</h3>
     <p>{address}</p>
-    <p>{time}</p>
+    {!!time && <p>{time}</p>}
     <p>{paymentMethod}</p>
   </div>
 );
